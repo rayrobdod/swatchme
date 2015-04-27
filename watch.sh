d13f9d674d453142c9d6ab7d7d4673e4aa375d30
Raymond Dodge <rrd1907@rit.edu>
2015-04-27 19:23:09 -0400

    Attempting this travis integration thing

--- watch.sh
+++ watch.sh
@@ -1,11 +1,7 @@
-#!/bin/sh
+!/usr/bin/env sh
 
-# Change `style.css` or `style.min.css` to whatever you would like your compiled
-# stylesheet to be called. Do not rename `style.scss` or alter references to it.
-
-# No minification
+npm install --save-dev qunitjs
+npm install sass
 sass --watch css/main.scss:css/main.css --style expanded
-
-# sass --watch assets/scss/markmichon.scss:assets/css/markmichon.min.css --style compressed
 
 exit 0
