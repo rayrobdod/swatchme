1a1735477bed61f89840b6e4c23671057c45bf21
Raymond Dodge <git@rayrobdod.name>
2015-05-01 16:46:14 -0400

    Because why would I wan the code-under-test to be accessible from the tests? That's pure madness right there.
    > ReferenceError: rgbToHex is not defined

--- js/tests.js
+++ js/tests.js
@@ -1,4 +1,7 @@
-QUnit.test( "hello test", function( assert ) {
-  assert.ok( 1 == "1", "Passed!" );
+QUnit.test( "rgbToHex(0,0,0)", function( assert ) {
+	assert.ok( '000000' == rgbToHex(0,0,0), hexVal );
 });
 
+QUnit.test( "rgbToHex(255,255,255)", function( assert ) {
+	assert.ok( 'FFFFFF' == rgbToHex(255,255,255) );
+});
